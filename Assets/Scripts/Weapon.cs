dceed5abb379e82d33689b74da7f781f6a1a8f51
Muzammil <muzammil.ab314@gmail.com>
2026-09-08 16:58:33 +0500

    Feat: Added Animator and Kickback Animation

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] private ParticleSystem muzzleFlash;
 
     private Camera mainCamera;
     private StarterAssets.StarterAssetsInputs input;
+
+    const string SHOOT_STRING = "Shoot";
 
     void Awake()
     {
@@ -17,8 +20,10 @@ public class Weapon : MonoBehaviour
     {
         if (input.shoot)
         {
-            // Play muzzle flash
+            
             muzzleFlash.Play();
+            animator.Play(SHOOT_STRING,0,0f);
+            input.ShootInput(false);
 
             Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
 
