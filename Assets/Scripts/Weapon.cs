9ec231ae541d5828266f2eb46ce878b21b6d4f2e
Muzammil <muzammil.ab314@gmail.com>
2026-09-11 15:34:46 +0500

    Feat: Added Recoil Through Cinemachine Impulse And Added Explosion VFX Prefab

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,13 +1,35 @@
 using UnityEngine;
+using Unity.Cinemachine;
 
 public class Weapon : MonoBehaviour
 {
     [SerializeField] private ParticleSystem muzzle;
     [SerializeField] private LayerMask raycastLayers;
 
+    private CinemachineImpulseSource impulseSource;
+
+    private void Awake()
+    {
+        impulseSource = GetComponent<CinemachineImpulseSource>();
+
+        if (impulseSource == null)
+        {
+            Debug.LogWarning(
+                $"No CinemachineImpulseSource found on '{gameObject.name}'."
+            );
+        }
+    }
+
     public void Shoot(WeaponSO weaponData)
     {
+        // Play muzzle flash.
         muzzle.Play();
+
+        // Fire camera impulse.
+        if (impulseSource != null)
+        {
+            impulseSource.GenerateImpulse();
+        }
 
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f));
 
@@ -22,7 +44,9 @@ public class Weapon : MonoBehaviour
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
 
             if (enemyHealth != null)
+            {
                 enemyHealth.TakeDamage(weaponData.Damage);
+            }
         }
     }
 }
