59e76ac6d1e4267986175391ae9808916fe57e6f
Muzammil Abubakar <muzammil.ab314@gmail.com>
2026-09-14 22:47:11 +0500

    Feat: Added Player Health And UI Coupling

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,24 +1,48 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Unity.Cinemachine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Range(1, 10)]
     [SerializeField] private int health = 5;
+
+    [SerializeField] private Image[] shieldBars;
 
     [SerializeField] private CinemachineCamera virtualCamera;
     [SerializeField] private Transform weaponCamera;
 
     [SerializeField] private int deathCameraPriority = 20;
 
+    private void Awake()
+    {
+        AdjustShieldUI();
+    }
+
     public void TakeDamage(int damage)
     {
         health -= damage;
 
-        Debug.Log(damage + " damage taken. Health remaining: " + health);
+        AdjustShieldUI();
 
         if (health <= 0)
         {
             HandleDeath();
+        }
+    }
+
+    private void AdjustShieldUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < health)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
         }
     }
 
